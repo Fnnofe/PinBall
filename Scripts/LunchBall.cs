851174615246c161a7ffa981d813ae142460cab8
Abdullah Bawazer <fn_nofe@yahoo.com>
2023-10-15 16:00:53 +0400

    Ball Launcher
    When pressing space the ball gets lunched and it has 2 seconds cooldown.

---
+++ Scripts/LunchBall.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class LunchBall : MonoBehaviour
+{
+    AreaEffector2D luncher;
+    float timer = 2;
+    float currentTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        luncher = GetComponent<AreaEffector2D>();
+        currentTime = 0;
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentTime > 0)
+        {
+            currentTime = currentTime - Time.deltaTime;
+            Debug.Log("currentTime: " + (int)currentTime);
+
+
+        }
+        else luncher.enabled = false;
+
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+            LunchBalls();
+        }
+
+
+    }
+    void LunchBalls()
+    {
+
+        luncher.enabled= true;
+        currentTime = timer;
+
+
+    }
+
+}
